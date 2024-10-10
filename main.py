e13da79ff713543f68080e41f745f70852424be6
knqti <166448899+knqti@users.noreply.github.com>
2024-10-10 11:58:04 -0700

    Complete check out function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import csv
-import datetime
 import os
+from datetime import datetime, timedelta
 
 
 def display_books(library_file):
@@ -8,37 +8,61 @@ def display_books(library_file):
         reader = csv.reader(file)
 
         # Display header section
-        print(f'\n{"Index":<5} | {"Author-Last":<20} | {"Author-First":<20} | {"Title":<19} | {"Edition":<8} | {"Status":<10} | {"Due Date":<10}')
-        print('-' * 110)
+        print(f'\n{"Index":<5} | {"Author-Last":<20} | {"Author-First":<20} | {"Title":<19} | {"Edition":<8} | {"Status":<15} | {"Due Date":<10}')
+        print('-' * 115)
 
         # Display library content
         for index, row in enumerate(reader):
-            print(f'{index:<5} | {row[0][:20]:<20} | {row[1][:20]:<20} | {row[2][:20]:<20}| {row[3]:<8} | {row[4]:<10} | {row[5]:<10}')
+            print(f'{index:<5} | {row[0][:20]:<20} | {row[1][:20]:<20} | {row[2][:20]:<20}| {row[3]:<8} | {row[4]:<15} | {row[5]:<10}')
 
 
 def check_out_book(library_file):
-    # confirmation = check_out_confirmation(library_file)
+    user_input = input('\n>>> Select the Index number to check out (x to main menu): ').strip().lower()
+
+    # Unhappy route
+    if user_input != 'x' and not 0<= int(user_input) <=3:
+        print('Invalid choice. Please try again.\nTo check out a book, type its Index number and press enter.\nTo go back to the main menu, type "x" and press enter.')
+        user_input = 'x'
+        return user_input
     
+    # Exit to main menu
+    if user_input == 'x':
+        return
+    
+    # Check out book
     with open(library_file, 'r') as file:
         reader = csv.reader(file)
+        updated_rows = []
 
-        user_check_out = input('\n>>> Select the Index number to check out (x to main menu): ').strip().lower()
+        for index, row in enumerate(reader):   
+            if index == int(user_input):
+                book_status = row[4]
 
-        for index, row in enumerate(reader):
-            
-            # Exit the function
-            if user_check_out == 'x':
-                return user_check_out
+                # Unhappy route
+                if book_status != 'Available':
+                    print('Sorry, that book is not available. Please try again.')
+                    user_input = 'x'
+                    return user_input
 
-            # Check out book
-            elif index == int(user_check_out):
                 print(f'\nChecking out:')
                 print(f'{row[2]} by {row[0]}, {row[1]}')
 
-                # UPDATE CSV FILE
+                for row in reader:
+                    book_status = 'Checked out'
+                    book_due_date = (datetime.now() + timedelta(days=14)).strftime('%Y-%m-%d')
 
-                return user_check_out
+                    row[4] = book_status
+                    row[5]= book_due_date
+                    print(f'\nYour book is due on {book_due_date}.')
 
+            updated_rows.append(row)
+
+    # Update the csv file
+    with open(library_file, 'w',newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(updated_rows)
+
+    user_input = 'success'
 
 
 def main():
@@ -55,7 +79,7 @@ def main():
         print('3. Return book')
         print('4. Exit')
         
-        user_navigation = input('>>> Select a number to navigate: ').strip()
+        user_navigation = input('\n>>> Select a number to navigate: ').strip()
 
         # Display books
         if user_navigation == '1':
@@ -64,12 +88,12 @@ def main():
 
         # Check out book
         elif user_navigation == '2':
-            user_choice = check_out_book(library_file)
+            check_out_result = check_out_book(library_file)
             
-            if user_choice == 'x':
+            if check_out_result == 'x':
                 continue
 
-            elif 1 <= int(user_choice) <= 4:
+            elif check_out_result == 'success':
                 print('\nCheck-out complete!')
                 continue
 
